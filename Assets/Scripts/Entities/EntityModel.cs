7a2abcc3303fc295bc45a9ada004fb1402d4f7e2
UltraLuke <lu.ez.ne@gmail.com>
2020-11-23 03:05:48 -0300

    Flocking agregado

--- Assets/Scripts/Entities/EntityModel.cs
+++ Assets/Scripts/Entities/EntityModel.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class EntityModel : MonoBehaviour
 {
-    [SerializeField] float speed;
-    [SerializeField] float speedRot;
+    [SerializeField] protected float speed;
+    [SerializeField] protected float speedRot;
 
-    public void Move(Vector3 dir)
+    public virtual void Move(Vector3 dir)
     {
         dir.y = 0;
-        transform.position += Time.deltaTime * dir * speed; ;
+        transform.position += Time.deltaTime * dir * speed;
         transform.forward = Vector3.Lerp(transform.forward, dir, speedRot * Time.deltaTime);
-        //_anim.SetFloat("Vel", 1);
     }
 }
---
+++ Assets/Scripts/Entities/HenchmanController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HenchmanController : MonoBehaviour
+{
+    Vector3 _dir = Vector3.zero;
+    EntityModel _entityModel;
+    private FlockEntity flock;
+
+    private void Awake()
+    {
+        flock = GetComponent<FlockEntity>();
+    }
+    void Start()
+    {
+        _entityModel = GetComponent<EntityModel>();
+    }
+    void Update()
+    {
+        _dir = flock.GetDir();
+        _entityModel.Move(_dir);
+    }
+}
